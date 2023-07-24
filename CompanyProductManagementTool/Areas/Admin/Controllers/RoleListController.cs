dbdeee27245598058ba4cde9189f3798ff11cb01
Pratik Bilonikar <pratik.bilonikar2020@gmail.com>
2023-07-24 10:34:51 +0530

    Pages added -New Role -Role List

---
+++ CompanyProductManagementTool/Areas/Admin/Controllers/RoleListController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMate.Areas.Admin.Controllers
+{
+    [Route("Admin")]
+    public class RoleListController : Controller
+    {
+        [Route("RoleList")]
+        public IActionResult RoleList()
+        {
+            return View();
+        }
+    }
+}
