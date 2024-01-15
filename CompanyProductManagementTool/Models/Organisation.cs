9342eb79604262775f7a28ba265cbb36174aaf40
Pratik Bilonikar <pratik.bilonikar2020@gmail.com>
2024-01-15 20:37:05 +0530

    Upgraded UIKIT version from 3.15.19 to 3.17.11

---
+++ CompanyProductManagementTool/Models/Organisation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMate.Models
+{
+    public class Organisation
+    {
+        public int intOranisationId { get; set; }
+        public string strOranisationName { get; set; }
+        public int intDelegateId { get; set; }
+        public DateTime dteContractFromDate { get; set; }
+        public DateTime dteContractToDate { get; set; }
+        public DateTime dteCreateDate { get; set; }
+        public int intCreatedBy { get; set; }
+        public int intOrganisationStatus { get; set; }
+    }
+}
